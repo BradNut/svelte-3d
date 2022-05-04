3dbb35264ecc837fd554d89fa5e795c1506014db
Bradley Shellnut <bradleyshellnut@protonmail.com>
2022-05-03 19:04:11 -0700

    feat: :fire: Adding 3D star GLTF file and messing with lighting and color

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,45 +1,29 @@
 <script>
   import * as THREE from 'three';
+  import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
   import * as SC from 'svelte-cubed';
-  import { spring } from 'svelte/motion';
+  let star;
+  let loader = new GLTFLoader();
 
-  let rows = 5;
-  let depth = 10;
-  let hue = Math.round(Math.random() * 360);
-
-  let contributions = spring(getContributions());
-
-  function refresh() {
-    $contributions = getContributions();
-    hue = Math.round(Math.random() * 360);
-  }
-
-  function getContributions() {
-    return new Array(rows * depth).fill(0).map((value) => {
-      return Math.round(Math.random() * 10);
-    });
-  }
+  loader.load('star.gltf', (gltf) => {
+    star = gltf.scene.children[0].geometry;
+  });
 </script>
 
-<div class="controls">
-  <button on:click={refresh}>Refresh</button>
-</div>
-
 <SC.Canvas antialias alpha>
-  {#each $contributions as contribution, index}
-    <SC.Mesh
-      scale={[1, contribution, 1]}
-      geometry={new THREE.BoxGeometry()}
-      material={new THREE.MeshStandardMaterial({
-        color: `hsl(${hue}, 50%, ${contribution * 10}%)`,
-      })}
-      position={[Math.floor(index / rows), contribution / 2, index % rows]}
-    />
-  {/each}
-  <SC.PerspectiveCamera position={[15, 15, 15]} />
+  <SC.Mesh
+    geometry={star}
+    rotation={[-90, 0, 0]}
+    material={new THREE.MeshStandardMaterial({
+      color: 0xffff00,
+      roughness: 0,
+      metalness: 0.7,
+    })}
+  />
+  <SC.PerspectiveCamera position={[3, 3, 3]} />
   <SC.OrbitControls />
-  <SC.AmbientLight intensity={0.5} />
-  <SC.DirectionalLight intensity={1} position={[3, 5, -4]} />
+  <SC.AmbientLight intensity={1} />
+  <SC.DirectionalLight intensity={0.5} position={[-3, 5, 7]} />
 </SC.Canvas>
 
 <style>
