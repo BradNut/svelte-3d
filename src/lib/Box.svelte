97d810592d9e89b791e37a9b86108ab1fa3045eb
Bradley Shellnut <bradleyshellnut@protonmail.com>
2022-05-05 10:36:35 -0700

    Added a cube with gravity and a plane to land on.

---
+++ src/lib/Box.svelte
@@ -0,0 +1,13 @@
+<script>
+  import * as SC from 'svelte-cubed';
+  import * as THREE from 'three';
+  import * as PE from 'svelte-cannon';
+
+  const position = PE.writableVec3(0,5,0);
+</script>
+
+<PE.Body mass={1} bind:position={$position}>
+  <PE.Box size={[1,1,1]} />
+</PE.Body>
+
+<SC.Mesh geometry={new THREE.BoxGeometry()} position={$position.toArray()} />
---
+++ src/routes/svg3D.svelte
@@ -0,0 +1,42 @@
+x<script>
+  import { onMount } from 'svelte';
+  import * as SC from 'svelte-cubed';
+  import * as THREE from 'three';
+  import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader';
+  import logo from '$lib/logo.svg?raw';
+
+  const svgLogo = new SVGLoader().parse(logo);
+  
+  let x = Math.PI;
+  let y = 0;
+  let z = 0;
+</script>
+
+<SC.Canvas alpha>
+  <SC.Group scale={0.05} rotation={[x, y, z]}>
+    {#each svgLogo.paths as logoPath}
+      <SC.Mesh
+        geometry={new THREE.ExtrudeGeometry(logoPath.toShapes(false), {
+          depth: 40
+        })}
+      />
+    {/each}
+  </SC.Group>
+  <SC.PerspectiveCamera position={[ 10, 0, 100 ]} />
+  <SC.OrbitControls />
+</SC.Canvas>
+
+<div class="controls">
+  <label for="x">X: <input id="x" name="x" type="range" bind:value={x} min={0} max={5} step={0.01} /></label>
+  <label for="y">Y: <input id="y" name="y" type="range" bind:value={y} min={0} max={5} step={0.01} /></label>
+  <label for="z">Z: <input id="z" name="z" type="range" bind:value={z} min={0} max={5} step={0.01} /></label>
+</div>
+
+<style>
+  .controls { 
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 10;
+  }
+</style>
