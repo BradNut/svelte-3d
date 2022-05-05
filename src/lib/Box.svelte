7759197b030bdadce618e413bc7212819721d3d2
Bradley Shellnut <bradleyshellnut@protonmail.com>
2022-05-05 11:30:38 -0700

    Add collision to the cubes and rotation to cubes.

--- src/lib/Box.svelte
+++ src/lib/Box.svelte
@@ -2,12 +2,20 @@
   import * as SC from 'svelte-cubed';
   import * as THREE from 'three';
   import * as PE from 'svelte-cannon';
+  import * as CANNON from 'cannon-es';
 
-  const position = PE.writableVec3(0,5,0);
+  const position = PE.writableVec3(0, 5, 0);
+  let rotation = PE.writableVec3(0, 0, 0);
+  let velocity = new CANNON.Vec3(1, 1, 1);
 </script>
 
-<PE.Body mass={1} bind:position={$position}>
-  <PE.Box size={[1,1,1]} />
+<PE.Body on:collide={(e) => console.log(e)} mass={1} bind:position={$position} bind:rotation={$rotation} {velocity}>
+  <PE.Box size={[0.5,0.5,0.5]} />
 </PE.Body>
 
-<SC.Mesh geometry={new THREE.BoxGeometry()} position={$position.toArray()} />
+<SC.Mesh
+  castShadow
+  rotation={$rotation.toArray()}
+  geometry={new THREE.BoxGeometry()}
+  position={$position.toArray()}
+/>
